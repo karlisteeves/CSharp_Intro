a696e03652892c16f0769b140eb1f316085a9659
Karli Steeves <steeves.karli@gmail.com>
2020-04-25 21:46:06 -0500

    Initial check-in of module PracticeSharp

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PracticeSharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Big smelly!");
+
+            int num = 5;
+            Console.WriteLine(num);
+            int num2 = 10;
+            Console.WriteLine(num2);
+
+
+
+            string yourName;
+            Console.WriteLine("What's your name?");
+            yourName = Console.ReadLine();
+
+            Console.WriteLine("Hello" + "{0}", yourName);
+          
+        }
+    }
+}
