87a3dde8edc471d67d7d1a0775592e7c4df9344d
Karli Steeves <steeves.karli@gmail.com>
2020-04-26 19:48:22 -0500

    Pushing up my C sharp practice work!

--- Program.cs
+++ Program.cs
@@ -15,12 +15,23 @@ namespace PracticeSharp
 
 
 
+
+            //ReadLine is supposed to take a user's input and apply it. It's not working for me for some reason...
+            //ReadLine method always returns a string. We must convert anything that's not a string, like int or double,
+           //with Convert.ToInt16/32/64. 
             string yourName;
             Console.WriteLine("What's your name?");
             yourName = Console.ReadLine();
 
             Console.WriteLine("Hello" + "{0}", yourName);
-          
+
+            int age = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("You are {0} years old", age);
+
+            int favNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your favorite number is {0}", favNum);
+//this is a test
+
         }
     }
 }
